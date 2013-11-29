2499b80555e40c08852559c546c5b5a0a8ea55d3
nlevashov <me@yandex.ru>
2013-11-29 11:25:28 +0400

    a little bit faster Kruskal's algo

--- algo/MST_Kruskal.h
+++ algo/MST_Kruskal.h
@@ -10,8 +10,13 @@ graph<E, V, INFO> MST_Kruskal (graph<E, V, INFO> & g) {
 	auto vertecies = g.vertecies();
 	std::multiset<typename graph<E, V, INFO>::edge> sorted_edges(edges.begin(), edges.end());
 	disjoint_set_data<V> dsd(vertecies);
+	int k = 1;
 	for (auto e : sorted_edges) {
-		if (dsd.merge(e.from()->name(), e.to()->name())) tree.push_edge(e.from()->name(), e.to()->name(), e.weight());
+		if (dsd.merge(e.from()->name(), e.to()->name())) {
+			tree.push_edge(e.from()->name(), e.to()->name(), e.weight());
+			++k;
+			if (k == g.size()) break;
+		}
 	}
 	for (auto i : vertecies) tree[i].flag = g[i].flag;
 	return tree;
